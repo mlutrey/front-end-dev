09ba942e726913190a3f45b2edc4cabfe1df49ad
Megan Lutrey <meganlutrey@gmail.com>
2023-12-12 20:59:37 -0500

    switched a boolean

--- assignment10/spec/menu.service.spec.js
+++ assignment10/spec/menu.service.spec.js
@@ -30,7 +30,7 @@ describe('testFunctionMenuItemExists', function () {
   it('should return false', function() {
     $httpBackend.whenGET(ApiBasePath + '/menu_items.json').respond({ "A": {"menu_items": [{"short_name": "A1"}, {"short_name": "A2"}] }});
     menuService.menuItemExists(testItem2).then(function(response) {
-      expect(response).toEqual(true);
+      expect(response).toEqual(false);
     });
     $httpBackend.flush();
   });
